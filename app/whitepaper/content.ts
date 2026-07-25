559b859d8c440138e99c6df088c4af3a26731f81
riz <241835037+chrizzy-x@users.noreply.github.com>
2026-07-25 20:20:26 +0100

    docs: add canonical whitepaper content loader

---
+++ app/whitepaper/content.ts
@@ -0,0 +1,12 @@
+import part01 from './content/part-01';
+import part02 from './content/part-02';
+import part03 from './content/part-03';
+import part04 from './content/part-04';
+import part05 from './content/part-05';
+import part06 from './content/part-06';
+import part07 from './content/part-07';
+import part08 from './content/part-08';
+import part09 from './content/part-09';
+import part10 from './content/part-10';
+
+export const AGENTOS_WHITEPAPER_MARKDOWN = [part01, part02, part03, part04, part05, part06, part07, part08, part09, part10].join('');
